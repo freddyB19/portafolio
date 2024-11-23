cc78fd59183d023d33e60ece1ed53fc0250310bd
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-23 12:59:43 -0400

    style: quitando descripción del header

--- src/pages/header/usuario_info.mjs
+++ src/pages/header/usuario_info.mjs
@@ -4,14 +4,6 @@ export const UsarioInfo = ({}) => {
 			<h1 class="text-center header-title mt-4">BACKEND DEVELOPER</h1>
 			<h1 class="text-center header-name">FREDDY BOLÍVAR</h1>
 			<p class="colors"></p>
-			<p class="font-monospace">
-				Soy un apasionado desarrollador backend con experiencia en la 
-				creación y mantenimiento de sistemas robustos. Mi enfoque se 
-				basa en diferentes aspectos, principalmente en el diseño y 
-				programación de componentes funcionales para simplificar y 
-				automatizar el desarrollo de software.
-			</p>
-
 		</div>
 	`
 }
