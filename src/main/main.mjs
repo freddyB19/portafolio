7b22a74589307ae8be920e3b5f50d2940ce55e26
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-23 13:19:58 -0400

    style: agregando aboutme al main

--- src/main/main.mjs
+++ src/main/main.mjs
@@ -1,7 +1,7 @@
 import { Header } from '../pages/header/main.mjs'
 import { Projects } from '../pages/projects/main.mjs'
 import { Technologies } from '../pages/technologies/main.mjs'
-
+import { Aboutme } from '../pages/aboutme/main.mjs'
 
 export const Main = ({}) => {
 	const root = document.querySelector("#root");
@@ -12,6 +12,8 @@ export const Main = ({}) => {
 		${Projects({})}
 
 		${Technologies({})}
+
+		${Aboutme({})}
 
 	`
 }
