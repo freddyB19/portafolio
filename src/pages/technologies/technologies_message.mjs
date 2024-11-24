6a7cd72fbe77556e77e107248f2a4a9889cff3c9
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-24 01:56:14 -0400

    style: cambiando el texto en TechnologiesMessage

--- src/pages/technologies/technologies_message.mjs
+++ src/pages/technologies/technologies_message.mjs
@@ -1,9 +1,9 @@
 export const TechnologiesMessage = ({}) => {
 	return `
-		<div class="mb-3 text-uppercase">
-			<h3 class="text-center technologies">
-				tecnologías
-			</h3>
-		</div>
-	`;
+	<div class="mb-3 text-uppercase">
+		<h3 class="text-center technologies">
+			Tecnologías
+		</h3>
+	</div>
+	`
 }
