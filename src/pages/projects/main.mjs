521f7475bc0d93e4101d5b4c887435376d71e19f
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-24 02:09:21 -0400

    add: agregando ProjectUlkit

--- src/pages/projects/main.mjs
+++ src/pages/projects/main.mjs
@@ -1,5 +1,7 @@
 import {list_projects} from "./projects.mjs"
 import {Project} from "../../utils/projects.mjs"
+import {ProjectUlkit} from "../../utils/projects.mjs"
+
 
 
 export const Projects = ({}) => {
@@ -7,11 +9,11 @@ export const Projects = ({}) => {
 		<div id="info-projects" style="margin-top: 4rem;">
 			<h2 class="text-center projects">Proyectos</h2>
 			<p class="colors"></p>
-			<div class="row row-cols-1 row-cols-md-3 g-4 mb-3 justify-content-center">
+			<div class="row row-cols-1 row-cols-md-3 mb-3 justify-content-evenly gap-3">
 				${
 					list_projects.map(project => 
 						`
-						${Project({project})}
+						${Project({project})}	
 						`
 					).join('')}
 
