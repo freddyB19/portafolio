23ea88608831dd6822d1448abe1d37ea7017ab04
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-25 20:53:11 -0400

    style: modificando la invocacion de propiedades del objeto 'technologies'

--- src/pages/technologies/technologies_info.mjs
+++ src/pages/technologies/technologies_info.mjs
@@ -4,19 +4,19 @@ export const TechnologiesInfo = ({}) => {
 	return `
 	<div class="mt-5">
 		<div class="d-flex justify-content-center gap-3">
-			${technologies.javascript}
-			${technologies.css}
-			${technologies.html5}
+			${technologies.javascript({})}
+			${technologies.css({})}
+			${technologies.html5({})}
 		</div>
 		<div class="d-flex justify-content-center alig-items-center gap-3">
-			${technologies.python}
-			${technologies.django}
-			${technologies.flask}
-			${technologies.fastapi}
+			${technologies.python({})}
+			${technologies.django({})}
+			${technologies.flask({})}
+			${technologies.fastapi({})}
 		</div>
 		<div class="d-flex justify-content-center gap-3">
-			${technologies.redis}
-			${technologies.postgresql}
+			${technologies.redis({})}
+			${technologies.postgresql({})}
 		</div>
 	</div>
 	`;
