bf94fcfc1c4d3b196c483657e5fd3c3b821acbe4
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-25 22:39:41 -0400

    style: agregando margin-top a technologies/Technologies

--- src/pages/technologies/main.mjs
+++ src/pages/technologies/main.mjs
@@ -3,7 +3,7 @@ import { TechnologiesMessage } from "./technologies_message.mjs"
 
 export const Technologies = ({}) => {
 	return `
-		<div class="row justify-content-center align-items-center mb-5">
+		<div class="row justify-content-center align-items-center mt-5 mb-5">
 			<div class="col-12 col-md-4">
 				${TechnologiesMessage({})}
 			</div>
