e2173ff76e5837884f98927452162ea88dae5107
freddyB19 <freddyybolivar2013@gmail.com>
2024-11-25 22:41:40 -0400

    style: ajustando contenido en technologies/TechnologiesInfo

--- src/pages/technologies/technologies_info.mjs
+++ src/pages/technologies/technologies_info.mjs
@@ -2,22 +2,20 @@ import { technologies } from "../../utils/technologies.mjs"
 
 export const TechnologiesInfo = ({}) => {
 	return `
-	<div class="mt-5">
-		<div class="d-flex justify-content-center gap-3">
-			${technologies.javascript({})}
-			${technologies.css({})}
-			${technologies.html5({})}
-		</div>
-		<div class="d-flex justify-content-center alig-items-center gap-3">
-			${technologies.python({})}
-			${technologies.django({})}
-			${technologies.flask({})}
-			${technologies.fastapi({})}
-		</div>
-		<div class="d-flex justify-content-center gap-3">
-			${technologies.redis({})}
-			${technologies.postgresql({})}
-		</div>
+	<div class="d-flex justify-content-center gap-4 mb-2">
+		${technologies.javascript({})}
+		${technologies.css({})}
+		${technologies.html5({})}
+	</div>
+	<div class="d-flex justify-content-center alig-items-center gap-4 mb-2">
+		${technologies.python({})}
+		${technologies.django({})}
+		${technologies.flask({})}
+		${technologies.fastapi({})}
+	</div>
+	<div class="d-flex justify-content-center gap-4 mb-2">
+		${technologies.redis({})}
+		${technologies.postgresql({})}
 	</div>
 	`;
 }
